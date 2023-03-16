9ca729c72a7d577a57b65245a8a81c3aa2c4ea33
Mohit <mohit.bisht@tothenew.com>
2023-03-16 17:04:37 +0530

    Multithreading Assignment 2

--- Multithreading2/src/Question1.java
+++ Multithreading2/src/Question1.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.concurrent.*;
+
+public class Question1{
+    public static void main(String[] args) throws Exception{
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Callable<String> callable = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return Thread.currentThread().getName();
+            }
+        };
+
+        Future<String> future = executor.submit(callable);
+        System.out.println(future.get());
+        executor.shutdown();
+
+
     }
 }
+
+
+//Output:
+//        pool-1-thread-1
--- Multithreading2/src/Question3.java
+++ Multithreading2/src/Question3.java
@@ -1,2 +1,45 @@
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Question3 {
+    public static void main(String[] args) throws InterruptedException{
+        List<Runnable> list = new ArrayList<>();
+        list.add(() -> System.out.println("Task 1"));
+        list.add(() -> System.out.println("Task 2"));
+        list.add(() -> System.out.println("Task 3"));
+
+        ExecutorService executor1 = Executors.newSingleThreadExecutor();
+        executeTasks(executor1, list);
+
+        ExecutorService executor2 = Executors.newCachedThreadPool();
+        executeTasks(executor2, list);
+
+        ExecutorService executor3 = Executors.newFixedThreadPool(2);
+        executeTasks(executor3, list);
+
+    }
+
+
+    private static void executeTasks(ExecutorService executor, List<Runnable> tasks) throws InterruptedException {
+        for (Runnable task : tasks) {
+            executor.submit(task);
+        }
+
+        executor.shutdown();
+    }
 }
+
+
+//Output:
+//        Task 1
+//        Task 2
+//        Task 1
+//        Task 2
+//        Task 3
+//        Task 1
+//        Task 3
+//        Task 2
+//        Task 3
+//
+//        Process finished with exit code 0
